d05f80f23635e185b8a1a6ea2a686d14d8ec72a7
saulms <105379798+saulms777@users.noreply.github.com>
2022-10-31 17:20:20 -0400

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,6 +1,12 @@
 class Constants:
     SCREEN_WIDTH: int = 600
     SCREEN_HEIGHT: int = 800
-    HEAD_GREEN: tuple[int, int, int] = (0, 153, 0)
-    BODY_GREEN: tuple[int, int, int] = (0, 204, 0)
+    TITLE_HEIGHT: int = 100
+    SIZE: int = 25
+    BORDER_GREEN: tuple[int, int, int] = (0, 153, 0)
+    DARK_GREEN: tuple[int, int, int] = (128, 255, 0)
+    LIGHT_GREEN: tuple[int, int, int] = (153, 255, 51)
+    HEAD_BLUE: tuple[int, int, int] = (0, 128, 255)
+    BODY_BLUE: tuple[int, int, int] = (51, 153, 255)
     APPLE_RED: tuple[int, int, int] = (255, 153, 153)
+    WHITE: tuple[int, int, int] = (255, 255, 255)
