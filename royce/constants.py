b04e4248b89564ae4f26283b3f1ddfb24a7e3877
roycek <roycemalikov@gmail.com>
2022-11-02 14:03:58 -0400

    Update constants.py

--- royce/constants.py
+++ royce/constants.py
@@ -8,8 +8,9 @@ class Constants:
     BORDER_GREEN: tuple[int, int, int] = (0, 153, 0)
     DARK_GREEN: tuple[int, int, int] = (128, 255, 0)
     LIGHT_GREEN: tuple[int, int, int] = (153, 255, 51)
-    PORTAL_ORANGE: tuple[int, int] = (255, 165, 0)
-    PORTAL_BLUE: tuple[int, int] = (0, 0, 255)
+    PORTAL_ORANGE: tuple[int, int, int] = (255, 165, 0)
+    PORTAL_BLUE: tuple[int, int, int] = (0, 0, 255)
+    BOMB_BROWN: tuple[int, int, int] = (128, 128, 128)
     DEATH_COLORS_HEAD: list[tuple[int, int, int]] = [(0, 153, 0), (76, 148, 0), (114, 141, 0), (146, 131, 0), 
                                                      (177, 117, 0), (206, 98, 0), (232, 70, 0), (255, 0, 0)]
     DEATH_COLORS_BODY: list[tuple[int, int, int]] = [(0, 204, 0), (101, 190, 0), (142, 174, 0), (174, 156, 0),
