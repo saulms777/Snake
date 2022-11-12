9544b2c5c1f0ce2c675ba2d9974a496ec7a723bc
saulms <105379798+saulms777@users.noreply.github.com>
2022-11-12 18:06:23 -0500

    Add files via upload

--- samuel/constants.py
+++ samuel/constants.py
@@ -1,29 +1,5 @@
-from pygame.locals import (
-    K_w,
-    K_s,
-    K_a,
-    K_d,
-    K_UP,
-    K_DOWN,
-    K_LEFT,
-    K_RIGHT,
-)
-
-
 class Constants:
     TICKS: int = 10
-
-    # key directions
-    KEY_DIRECTIONS: dict[int, tuple[int, int]] = {
-        K_w: (0, -25),
-        K_s: (0, 25),
-        K_a: (-25, 0),
-        K_d: (25, 0),
-        K_UP: (0, -25),
-        K_DOWN: (0, 25),
-        K_LEFT: (-25, 0),
-        K_RIGHT: (25, 0)
-    }
 
     # colours
     HEAD_BLUE: tuple[int, int, int] = (0, 128, 255)
