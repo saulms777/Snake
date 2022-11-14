3eace1cea68390cdf4e5e4ae88f274884adb7654
saulms <105379798+saulms777@users.noreply.github.com>
2022-11-14 10:04:05 -0500

    Add files via upload

--- samuel/constants.py
+++ samuel/constants.py
@@ -4,6 +4,4 @@ class Constants:
     # colours
     HEAD_BLUE: tuple[int, int, int] = (0, 128, 255)
     BODY_BLUE: tuple[int, int, int] = (51, 153, 255)
-    DARK_GREEN: tuple[int, int, int] = (128, 255, 0)
-    LIGHT_GREEN: tuple[int, int, int] = (153, 255, 51)
     BLACK: tuple[int, int, int] = (0, 0, 0)
--- samuel/game.py
+++ samuel/game.py
@@ -13,16 +13,17 @@ class Game(Constants):
         # set screen and background
         self.screen = py.display.set_mode((600, 800))
         self.game_bg = py.image.load("images/game_bg.png").convert()
+        self.apple_points = py.image.load("images/apple_points.png").convert()
 
         # set font
         self.text = py.font.SysFont("calibri", 24, bold=True)
 
         # set snake default position
-        self.snake: list[list[int, int]] = [[350, 450],
-                                            [325, 450],
-                                            [300, 450],
-                                            [275, 450],
-                                            [250, 450]]
+        self.snake: list[list[int, int]] = [[350, 425],
+                                            [325, 425],
+                                            [300, 425],
+                                            [275, 425],
+                                            [250, 425]]
 
         # create snake objects
         self.head = py.Surface((25, 25))
@@ -68,7 +69,7 @@ class Game(Constants):
         if self.new_position not in self.snake:
 
             # check if hit a wall
-            if 25 <= self.new_position[0] <= 550 and 125 <= self.new_position[1] <= 750:
+            if 25 <= self.new_position[0] <= 550 and 75 <= self.new_position[1] <= 750:
                 return
 
         self.game_over = True
@@ -82,7 +83,7 @@ class Game(Constants):
         loop = True
         while loop:
             apple_x = randrange(25, 575, 25)
-            apple_y = randrange(125, 775, 25)
+            apple_y = randrange(75, 775, 25)
             if [apple_x, apple_y] not in [el for el in self.snake]:
                 loop = False
 
@@ -91,7 +92,9 @@ class Game(Constants):
     # draw background
     def draw_bg(self) -> None:
 
-        self.screen.blit(self.game_bg, (0, 100))
+        self.screen.blit(self.game_bg, (0, 0))
+        self.screen.blit(self.apple_points, (25, 25))
+        self.screen.blit(self.text.render(f"{self.points}", True, self.BLACK), (55, 27))
 
     # update snake list
     def update_snake(self) -> None:
@@ -123,10 +126,10 @@ class Game(Constants):
 
         # darken screen
         end_screen = py.image.load("images/ending_bg.png").convert()
-        self.screen.blit(end_screen, (0, 100))
+        self.screen.blit(end_screen, (0, 0))
 
         # end screen
-        self.screen.blit(self.apple_dark, (276, 400))
-        self.screen.blit(self.text.render(f"{self.points}", True, self.BLACK), (306, 403))
-        self.screen.blit(self.text.render("Press esc to", True, self.BLACK), (242, 475))
-        self.screen.blit(self.text.render("play again", True, self.BLACK), (250, 500))
+        self.screen.blit(self.apple_dark, (276, 350))
+        self.screen.blit(self.text.render(f"{self.points}", True, self.BLACK), (306, 353))
+        self.screen.blit(self.text.render("Press esc to", True, self.BLACK), (242, 425))
+        self.screen.blit(self.text.render("play again", True, self.BLACK), (250, 450))
